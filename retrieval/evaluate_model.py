688f91a9d56bdd98d8105c583d2f5e2e4ce8b9cb
Nthakur20 <nandant@gmail.com>
2021-01-24 17:56:12 +0100

    Intial commit

--- retrieval/evaluate_model.py
+++ retrieval/evaluate_model.py
@@ -1,7 +1,7 @@
 from loaders.data_loader import GenericDataLoader
 from evaluation.evaluation import EvaluateRetrieval
 
-data_path = "datasets/nfcorpus"
+data_path = "../datasets/nfcorpus"
 
 corpus, queries, qrels = GenericDataLoader(data_path).load(split="test")
 retriever = EvaluateRetrieval(model="sbert", model_name="distilroberta-base-paraphrase-v1")

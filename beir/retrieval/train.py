e30f61ae66729f0a36ad9550ff72d8679c52b59e
Nthakur20 <nandant@gmail.com>
2021-02-06 15:05:53 +0100

    updated training endpoints and ir_evaluator

--- beir/retrieval/train.py
+++ beir/retrieval/train.py
@@ -1,77 +1,130 @@
-from sentence_transformers import SentenceTransformer, SentencesDataset, models, losses
-from sentence_transformers.evaluation import SequentialEvaluator, InformationRetrievalEvaluator
+from sentence_transformers import SentenceTransformer, SentencesDataset, models
+from sentence_transformers.evaluation import SentenceEvaluator, SequentialEvaluator, InformationRetrievalEvaluator
 from sentence_transformers.readers import InputExample
+from transformers import AdamW
+from torch import nn
 from torch.utils.data import DataLoader
+from torch.optim import Optimizer
 from tqdm.autonotebook import trange
+from typing import Dict, Type, List, Callable, Iterable, Tuple
 import logging
 import time
+import random
 
 logger = logging.getLogger(__name__)
 
 class TrainRetriever:
     
-    def __init__(self, model_name, model_save_path, batch_size=64, max_seq_length=350):
+    def __init__(self, model_name: str, batch_size: int = 64, max_seq_length: int = 350):
         self.model_name = model_name
         self.batch_size = batch_size
-        self.model_save_path = model_save_path
+        self.max_seq_length = max_seq_length
 
-        word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
+        word_embedding_model = models.Transformer(model_name, max_seq_length=self.max_seq_length)
         pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
         self.model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
     
-    def load_train(self, corpus, queries, qrels):
+    def load_train(self, corpus: Dict[str, Dict[str, str]], queries: Dict[str, str], 
+                   qrels: Dict[str, Dict[str, int]]) -> List[Type[InputExample]]:
+        
         query_ids = list(queries.keys())
         train_samples = []
-        count = 0
 
-        for start_idx in trange(0, len(query_ids), self.batch_size, desc='Adding Input Examples'):
+        for idx, start_idx in enumerate(trange(0, len(query_ids), self.batch_size, desc='Adding Input Examples')):
             query_ids_batch = query_ids[start_idx:start_idx+self.batch_size]
             for query_id in query_ids_batch:
                 for corpus_id, score in qrels[query_id].items():
                     if score >= 1: # if score = 0, we don't consider for training
                         s1 = queries[query_id]
                         s2 = corpus[corpus_id]
-                        count += 1
-                        train_samples.append(InputExample(guid=count, texts=[s1, s2], label=1))
+                        train_samples.append(InputExample(guid=idx, texts=[s1, s2], label=1))
 
         logger.info("Loaded {} training pairs.".format(len(train_samples)))
         return train_samples
     
-    def load_dev(self, dev_corpus, dev_queries, dev_qrels, name="eval"):
-
-        # If no dev set is present, return the dummy evaluator
-        if len(self.dev_queries) <= 0:
-            return SequentialEvaluator([], main_score_function=lambda x: time.time())
+    def prepare_train(self, train_samples: List[Type[InputExample]], shuffle: bool = True) -> DataLoader:
         
-        dev_rel_docs = {}
+        train_data = SentencesDataset(train_samples, model=self.model)
+        train_dataloader = DataLoader(train_data, shuffle=shuffle, batch_size=self.batch_size)
+        return train_dataloader
+    
+    def load_ir_evaluator(self, corpus: Dict[str, Dict[str, str]], queries: Dict[str, str], 
+                 qrels: Dict[str, Dict[str, int]], max_corpus_size: int = None, name: str = "eval") -> SentenceEvaluator:
+
+        if len(queries) <= 0:
+            raise ValueError("Dev Set Empty!, Cannot evaluate on Dev set.")
+        
+        rel_docs = {}
+        corpus_ids = set()
+        
+        # need to convert corpus to cid => doc      
+        corpus = {idx: corpus[idx].get("text") + " " + corpus[idx].get("title") for idx in corpus}
+        
         # need to convert dev_qrels to qid => Set[cid]        
-        for query_id, metadata in dev_qrels.items():
-            dev_rel_docs[query_id] = set()
+        for query_id, metadata in qrels.items():
+            rel_docs[query_id] = set()
             for corpus_id, score in metadata.items():
                 if score >= 1:
-                    dev_rel_docs[query_id].add(corpus_id)
-
-        return InformationRetrievalEvaluator(dev_queries, dev_corpus, dev_rel_docs, name=name)
-
-
-    def train(self, train_samples, evaluator=None, num_epochs=1, lr=2e-5, evaluation_steps=5000):
-
-        train_data = SentencesDataset(train_samples, model=self.model)
-        train_dataloader = DataLoader(train_data, shuffle=True, batch_size=self.batch_size)
-        train_loss = losses.MultipleNegativesRankingLoss(model=self.model)
-        if not evaluator: # dummy evaluator
-            evaluator = SequentialEvaluator([], main_score_function=lambda x: time.time())
+                    corpus_ids.add(corpus_id)
+                    rel_docs[query_id].add(corpus_id)
+        
+        if max_corpus_size:
+            # check if length of corpus_ids > max_corpus_size
+            if len(corpus_ids) > max_corpus_size:
+                raise ValueError("Your maximum corpus size should atleast contain {} corpus ids".format(len(corpus_ids)))
             
-        warmup_steps = int(len(train_samples) * num_epochs / self.batch_size * 0.1)
+            # Add mandatory corpus documents
+            new_corpus = {idx: corpus[idx] for idx in corpus_ids}
+            
+            # Remove mandatory corpus documents from original corpus
+            for corpus_id in corpus_ids:
+                corpus.pop(corpus_id, None)
+            
+            # Sample randomly remaining corpus documents
+            for corpus_id in random.sample(list(corpus), max_corpus_size - len(corpus_ids)):
+                new_corpus[corpus_id] = corpus[corpus_id]
+
+            corpus = new_corpus
+
+        logger.info("{} set contains {} documents and {} queries".format(name, len(corpus), len(queries)))
+        return InformationRetrievalEvaluator(queries, corpus, rel_docs, name=name)
+    
+    def load_dummy_evaluator(self) -> SentenceEvaluator:
+            return SequentialEvaluator([], main_score_function=lambda x: time.time())
+
+    def fit(self, 
+            train_objectives: Iterable[Tuple[DataLoader, nn.Module]],
+            evaluator: SentenceEvaluator = None,
+            epochs: int = 1,
+            steps_per_epoch = None,
+            scheduler: str = 'WarmupLinear',
+            warmup_steps: int = 10000,
+            optimizer_class: Type[Optimizer] = AdamW,
+            optimizer_params : Dict[str, object]= {'lr': 2e-5, 'eps': 1e-6, 'correct_bias': False},
+            weight_decay: float = 0.01,
+            evaluation_steps: int = 0,
+            output_path: str = None,
+            save_best_model: bool = True,
+            max_grad_norm: float = 1,
+            use_amp: bool = False,
+            callback: Callable[[float, int, int], None] = None
+            ):
         
         # Train the model
         logger.info("Starting to Train...")
-        self.model.fit(train_objectives=[(train_dataloader, train_loss)],
+
+        self.model.fit(train_objectives=train_objectives,
                 evaluator=evaluator,
-                epochs=num_epochs,
+                epochs=epochs,
+                steps_per_epoch=steps_per_epoch,
                 warmup_steps=warmup_steps,
-                optimizer_params={'lr': lr},
-                output_path=self.model_save_path,
+                optimizer_class=optimizer_class,
+                scheduler=scheduler,
+                optimizer_params=optimizer_params,
+                weight_decay=weight_decay,
+                output_path=output_path,
                 evaluation_steps=evaluation_steps,
-                use_amp=True
-                )
+                save_best_model=save_best_model,
+                max_grad_norm=max_grad_norm,
+                use_amp=use_amp,
+                callback=callback)
--- examples/generation/query_gen_and_train.py
+++ examples/generation/query_gen_and_train.py
@@ -3,6 +3,7 @@ from beir.datasets.data_loader import GenericDataLoader
 from beir.generation import QueryGenerator as QGen
 from beir.generation.models import QGenModel
 from beir.retrieval.train import TrainRetriever
+from sentence_transformers import losses
 
 import pathlib, os
 import logging
@@ -44,15 +45,31 @@ dev_corpus, dev_queries, dev_qrels = GenericDataLoader(data_path).load(split="de
 
 #### Provide any sentence-transformers model path
 model_name = "distilroberta-base"
+retriever = TrainRetriever(model_name=model_name, batch_size=64)
+
+#### Prepare training samples
+train_samples = retriever.load_train(corpus, queries, qrels)
+train_dataloader = retriever.prepare_train(train_samples, shuffle=True)
+train_loss = losses.MultipleNegativesRankingLoss(model=retriever.model)
+
+#### Prepare dev evaluator
+ir_evaluator = retriever.load_ir_evaluator(dev_corpus, dev_queries, dev_qrels)
+#### If no dev set is present
+# ir_evaluator = retriever.load_dummy_evaluator()
+
+#### Provide model save path
 model_save_path = os.path.join(pathlib.Path(__file__).parent.absolute(), "output", "{}-genQ-nfcorpus".format(model_name))
 os.makedirs(model_save_path, exist_ok=True)
 
-retriever = TrainRetriever(model_name=model_name, model_save_path=model_save_path)
-train_samples = retriever.load_train(corpus, gen_queries, gen_qrels)
-ir_evaluator = retriever.load_dev(dev_corpus, dev_queries, dev_qrels)
-
-#### Train the model
-
+#### Configure Train params
 num_epochs = 1
 evaluation_steps = 5000
-results = retriever.train(train_samples, evaluator=ir_evaluator, num_epochs=num_epochs, evaluation_steps=evaluation_steps)
+warmup_steps = int(len(train_samples) * num_epochs / retriever.batch_size * 0.1)
+
+retriever.fit(train_objectives=[(train_dataloader, train_loss)], 
+                evaluator=ir_evaluator, 
+                epochs=num_epochs,
+                output_path=model_save_path,
+                warmup_steps=warmup_steps,
+                evaluation_steps=evaluation_steps,
+                use_amp=True)

29895a548d349d4a594ab2a7c0816379135c2f89
Nandan Thakur <thakur@ukp.informatik.tu-darmstadt.de>
2021-04-19 13:25:27 +0200

    added sep and two-tower architectures if available

--- beir/retrieval/models/__init__.py
+++ beir/retrieval/models/__init__.py
@@ -1,3 +1,2 @@
 from .sentence_bert import SentenceBERT
-from .dpr import DPR
 from .use_qa import UseQA
--- beir/retrieval/models/sentence_bert.py
+++ beir/retrieval/models/sentence_bert.py
@@ -1,14 +1,23 @@
 from sentence_transformers import SentenceTransformer
+from torch import Tensor
+from typing import List, Dict, Union, Tuple
 import numpy as np
-from typing import List, Dict
 
 class SentenceBERT:
-    def __init__(self, model_path=None, **kwargs):
-        self.model = SentenceTransformer(model_path)
+    def __init__(self, model_path: Union[str, Tuple] = None, sep: str = " ", **kwargs):
+        self.sep = sep
+        
+        if isinstance(model_path, str):
+            self.q_model = SentenceTransformer(model_path)
+            self.doc_model = self.q_model
+        
+        elif isinstance(model_path, tuple):
+            self.q_model = SentenceTransformer(model_path[0])
+            self.doc_model = SentenceTransformer(model_path[1])
     
-    def encode_queries(self, queries: List[str], batch_size: int = 16, **kwargs) -> np.ndarray:
-        return np.asarray(self.model.encode(queries, batch_size=batch_size, **kwargs))
+    def encode_queries(self, queries: List[str], batch_size: int = 16, **kwargs) -> Union[List[Tensor], np.ndarray, Tensor]:
+        return self.q_model.encode(queries, batch_size=batch_size, **kwargs)
     
-    def encode_corpus(self, corpus: List[Dict[str, str]], batch_size: int = 8, **kwargs) -> np.ndarray:
-        sentences = [(doc["title"] + "  " + doc["text"]).strip() for doc in corpus]
-        return np.asarray(self.model.encode(sentences, batch_size=batch_size, **kwargs))
+    def encode_corpus(self, corpus: List[Dict[str, str]], batch_size: int = 8, **kwargs) -> Union[List[Tensor], np.ndarray, Tensor]:
+        sentences = [(doc["title"] + self.sep + doc["text"]).strip() for doc in corpus]
+        return self.doc_model.encode(sentences, batch_size=batch_size, **kwargs)

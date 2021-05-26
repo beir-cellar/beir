ecf1a891e8a4089ce9110427b7a71a4dbb9c6db2
NThakur20 <nandant@gmail.com>
2021-05-26 18:18:58 +0200

    add: feature to view progress using tqdm when downloading files

--- beir/util.py
+++ beir/util.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from tqdm.autonotebook import tqdm
 import csv
 import torch
 import json
@@ -28,12 +29,28 @@ def dot_score(a: torch.Tensor, b: torch.Tensor):
 
     return torch.mm(a, b.transpose(0, 1))
 
-def download_url(url: str, save_path: str, chunk_size: int = 128):
+def download_url(url: str, save_path: str, chunk_size: int = 1024):
+    """Download url with progress bar using tqdm
+    https://stackoverflow.com/questions/15644964/python-progress-bar-and-downloads
+
+    Args:
+        url (str): downloadable url
+        save_path (str): local path to save the downloaded file
+        chunk_size (int, optional): chunking of files. Defaults to 1024.
+    """
     if not os.path.isfile(save_path):
         r = requests.get(url, stream=True)
-        with open(save_path, 'wb') as fd:
-            for chunk in r.iter_content(chunk_size=chunk_size):
-                fd.write(chunk)
+        total = int(r.headers.get('Content-Length', 0))
+        with open(save_path, 'wb') as fd, tqdm(
+            desc=save_path,
+            total=total,
+            unit='iB',
+            unit_scale=True,    
+            unit_divisor=chunk_size,
+        ) as bar:
+            for data in r.iter_content(chunk_size=chunk_size):
+                size = fd.write(data)
+                bar.update(size)
 
 def unzip(zip_file: str, out_dir: str):
     if not os.path.isdir(zip_file.replace(".zip", "")):
@@ -41,14 +58,14 @@ def unzip(zip_file: str, out_dir: str):
         zip_.extractall(path=out_dir)
         zip_.close()
 
-def download_and_unzip(url: str, out_dir: str) -> str:
+def download_and_unzip(url: str, out_dir: str, chunk_size: int = 1024) -> str:
     
     os.makedirs(out_dir, exist_ok=True)
     dataset = url.split("/")[-1]
     zip_file = os.path.join(out_dir, dataset)
     
     logger.info("Downloading {} ...".format(dataset))
-    download_url(url, zip_file)
+    download_url(url, zip_file, chunk_size)
     
     logger.info("Unzipping {} ...".format(dataset))
     unzip(zip_file, out_dir)

7849f913cc671f94071d6392a7dd4a623635e622
abhesrivas <abhesrivastava@gmail.com>
2021-06-24 17:36:57 +0200

    add: cosine similarity function in util

--- beir/util.py
+++ beir/util.py
@@ -29,6 +29,27 @@ def dot_score(a: torch.Tensor, b: torch.Tensor):
 
     return torch.mm(a, b.transpose(0, 1))
 
+def cos_sim(a: torch.Tensor, b: torch.Tensor):
+    """
+    Computes the cosine similarity cos_sim(a[i], b[j]) for all i and j.
+    :return: Matrix with res[i][j]  = cos_sim(a[i], b[j])
+    """
+    if not isinstance(a, torch.Tensor):
+        a = torch.tensor(a)
+
+    if not isinstance(b, torch.Tensor):
+        b = torch.tensor(b)
+
+    if len(a.shape) == 1:
+        a = a.unsqueeze(0)
+
+    if len(b.shape) == 1:
+        b = b.unsqueeze(0)
+
+    a_norm = torch.nn.functional.normalize(a, p=2, dim=1)
+    b_norm = torch.nn.functional.normalize(b, p=2, dim=1)
+    return torch.mm(a_norm, b_norm.transpose(0, 1))
+
 def download_url(url: str, save_path: str, chunk_size: int = 1024):
     """Download url with progress bar using tqdm
     https://stackoverflow.com/questions/15644964/python-progress-bar-and-downloads

4f4048147f619bb17f88cb69d660d2793c35ca62
abhesrivas <abhesrivastava@gmail.com>
2021-06-24 17:37:33 +0200

    fix: corrected faiss indexing for binary hashed retrieval

--- beir/retrieval/search/dense/binary_search.py
+++ beir/retrieval/search/dense/binary_search.py
@@ -4,6 +4,7 @@ import logging
 import sys
 import torch
 import faiss
+import numpy as np
 from typing import Dict, List
 
 logger = logging.getLogger(__name__)
@@ -24,36 +25,43 @@ class DenseRetrievalBinaryCodeSearch:
     def _create_mapping_ids(self, corpus_ids):
         if not all(isinstance(doc_id, int) for doc_id in corpus_ids):
             for idx in range(len(corpus_ids)):
-                self.rev_mapping[idx] = corpus_ids[idx]
                 self.mapping[corpus_ids[idx]] = idx
+                self.rev_mapping[idx] = corpus_ids[idx]
     
-    def index(self, corpus: Dict[str, Dict[str, str]], hash_num_bits: int = 768):
+    def index(self, corpus: Dict[str, Dict[str, str]], hash_num_bits: int = 768, output_dir: str = None):
         
         logger.info("Encoding Corpus in batches... Warning: This might take a while!")
         corpus_ids = list(corpus.keys())
         self._create_mapping_ids(corpus_ids)
+
         corpus = [corpus[cid] for cid in corpus_ids]
-        
+
         itr = range(0, len(corpus), self.corpus_chunk_size)
 
         for batch_num, corpus_start_idx in enumerate(itr):
             logger.info("Encoding Batch {}/{}...".format(batch_num+1, len(itr)))
             corpus_end_idx = min(corpus_start_idx + self.corpus_chunk_size, len(corpus))
-            sub_corpus_ids = [self.mapping[idx] for idx in corpus_ids[corpus_start_idx:corpus_end_idx]]
             
             #Encode chunk of corpus    
-            sub_corpus_binary_codes = self.model.encode_corpus(
+            sub_corpus_embeddings = self.model.encode_corpus(
                 corpus[corpus_start_idx:corpus_end_idx],
                 show_progress_bar=self.show_progress_bar, 
                 batch_size=self.batch_size)
             
-            #Index chunk of corpus into faiss
-            logger.info("Indexing Batch {}/{} into Faiss...".format(batch_num+1, len(itr)))
-            dim_size = sub_corpus_binary_codes.shape[1]
-            base_index = faiss.IndexBinaryHash(dim_size * 8, hash_num_bits)
-            self.faiss_index = FaissBinaryIndex.build(sub_corpus_ids, sub_corpus_binary_codes, base_index)
+            if not batch_num: 
+                corpus_embeddings = sub_corpus_embeddings
+            else:
+                corpus_embeddings = np.vstack([corpus_embeddings, sub_corpus_embeddings])
         
-        del sub_corpus_ids, sub_corpus_binary_codes
+        #Index chunk of corpus into faiss
+        logger.info("Indexing Passages into Faiss...") 
+        
+        base_index = faiss.IndexBinaryHash(corpus_embeddings.shape[1] * 8, hash_num_bits)
+        faiss_ids = [self.mapping.get(corpus_id) for corpus_id in corpus_ids]
+        self.faiss_index = FaissBinaryIndex.build(faiss_ids, corpus_embeddings, base_index)
+        
+        logger.info("Faiss indexing completed! {} Documents Indexed...".format(len(self.faiss_index._passage_ids)))
+        del sub_corpus_embeddings, corpus_embeddings
         
     
     def search(self, 
@@ -66,7 +74,7 @@ class DenseRetrievalBinaryCodeSearch:
                index: bool = True, **kwargs) -> Dict[str, Dict[str, float]]:
         
         ## Used for Indexing
-        if index: self.index(corpus)
+        if index: self.index(corpus, **kwargs)
 
         logger.info("Encoding Queries...")
         query_ids = list(queries.keys())
@@ -74,14 +82,14 @@ class DenseRetrievalBinaryCodeSearch:
         query_embeddings = self.model.encode_queries(
             queries, show_progress_bar=self.show_progress_bar, batch_size=self.batch_size)
 
-        scores, doc_ids = self.faiss_index.search(query_embeddings, top_k, binary_k=binary_k, rerank=rerank)
+        faiss_scores, faiss_doc_ids = self.faiss_index.search(query_embeddings, top_k, binary_k=binary_k, rerank=rerank)
         
         for idx in range(len(query_ids)):
-            _scores = [float(score) for score in scores[idx]]
+            scores = [float(score) for score in faiss_scores[idx]]
             if len(self.rev_mapping) != 0:
-                _doc_ids = [self.rev_mapping[doc_id] for doc_id in doc_ids[idx]]
+                doc_ids = [self.rev_mapping[doc_id] for doc_id in faiss_doc_ids[idx]]
             else:
-                _doc_ids = [str(doc_id) for doc_id in doc_ids[idx]]
-            self.results[query_ids[idx]] = dict(zip(_doc_ids, _scores))
+                doc_ids = [str(doc_id) for doc_id in faiss_doc_ids[idx]]
+            self.results[query_ids[idx]] = dict(zip(doc_ids, scores))
         
         return self.results

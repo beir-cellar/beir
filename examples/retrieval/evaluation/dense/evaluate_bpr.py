40e5ab88d19faf06de129f095be91f640c7e1ccd
NThakur20 <nandant@gmail.com>
2021-07-05 09:19:48 +0200

    fix: completely changed faiss index example and bpr example

--- examples/retrieval/evaluation/dense/evaluate_bpr.py
+++ examples/retrieval/evaluation/dense/evaluate_bpr.py
@@ -2,7 +2,7 @@ from beir import util, LoggingHandler
 from beir.retrieval import models
 from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.evaluation import EvaluateRetrieval
-from beir.retrieval.search.dense import DenseRetrievalBinaryCodeSearch as DRBCS
+from beir.retrieval.search.dense import BinaryFaissSearch
 
 import logging
 import pathlib, os
@@ -35,20 +35,47 @@ corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="te
 # The model was fine-tuned using CLS Pooling and dot-product!
 # Open-sourced binary code SBERT model trained on MSMARCO to be made available soon!
 
-model_path = "distilbert-base-uncased" # To be coming soon! 
-model = DRBCS(models.BinarySentenceBERT(model_path), batch_size=16)
-retriever = EvaluateRetrieval(model)
+model = models.BinarySentenceBERT("msmarco-distilbert-base-tas-b") # Proxy for now, soon coming up BPR models trained on MSMARCO!
+faiss_search = BinaryFaissSearch(model, batch_size=128)
 
-# BPR first retrieves binary_k (default 1000) documents based on query hash and document hash similarity with hamming distance.
-# The hamming distance similarity is constructed using IndexBinaryHash in Faiss: https://faiss.ai/cpp_api/struct/structfaiss_1_1IndexBinaryHash.html
-# BPR then reranks with similarity b/w query embedding and the documents hashes for these binary_k documents.
-# Reranking is advised as its quite fast, efficient and leads to decent performances.
+#### Load faiss index from file or disk ####
+# We need two files to be present within the input_dir!
+# 1. input_dir/my-index.bin.faiss ({prefix}.{ext}.faiss) which loads the faiss index.
+# 2. input_dir/my-index.bin.tsv ({prefix}.{ext}.faiss) which loads mapping of ids i.e. (beir-doc-id \t faiss-doc-id).
+
+prefix = "my-index"       # (default value)
+ext = "bin"               # bin for binary (default value)
+input_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "faiss-index")
+
+if os.path.isdir(input_dir):
+    faiss_search.load(input_dir=input_dir, prefix=prefix, ext=ext)
+
+# BPR first retrieves binary_k (default 1000) documents based on query hash and document hash similarity with hamming distance!
+# The hamming distance similarity is constructed using IndexBinaryFlat in Faiss.
+# BPR then reranks with dot similarity b/w query embedding and the documents hashes for these binary_k documents.
+# Please Note, Reranking here is done with a bi-encoder which is quite faster compared to cross-encoders.
+# Reranking is advised by the original paper as its quite fast, efficient and leads to decent performances.
+
+score_function = "dot" # or cos_sim for cosine similarity
+retriever = EvaluateRetrieval(faiss_search, score_function=score_function)
 
 rerank = True                       # False would only retrieve top-k documents based on hamming distance.
 binary_k = 1000                     # binary_k value denotes documents reranked for each query.
-index = True                        # True would index all documents first to faiss (faiss::IndexBinaryHash).
 
-results = retriever.retrieve(corpus, queries, rerank=rerank, binary_k=binary_k, index=index)
+results = retriever.retrieve(corpus, queries, rerank=rerank, binary_k=binary_k)
+
+### Save faiss index into file or disk ####
+# Unfortunately faiss only supports integer doc-ids!
+# This will mean we need save two files in your output_dir path =>
+# 1. output_dir/my-index.bin.faiss ({prefix}.{ext}.faiss) which saves the faiss index.
+# 2. output_dir/my-index.bin.tsv ({prefix}.{ext}.faiss) which saves mapping of ids i.e. (beir-doc-id \t faiss-doc-id).
+
+prefix = "my-index"
+ext = "bin"
+output_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "faiss-index")
+
+os.makedirs(output_dir, exist_ok=True)
+faiss_search.save(output_dir=output_dir, prefix=prefix, ext=ext)
 
 #### Evaluate your retrieval using NDCG@k, MAP@K ...
 
--- examples/retrieval/evaluation/dense/evaluate_faiss_dense.py
+++ examples/retrieval/evaluation/dense/evaluate_faiss_dense.py
@@ -2,10 +2,11 @@ from beir import util, LoggingHandler
 from beir.retrieval import models
 from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.evaluation import EvaluateRetrieval
-from beir.retrieval.search.dense import DenseRetrievalFaissSearch as DRFS
+from beir.retrieval.search.dense import PQFaissSearch, HNSWFaissSearch, FlatIPFaissSearch
 
-import pathlib, os
 import logging
+import pathlib, os
+import random
 
 #### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -14,38 +15,98 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
                     handlers=[LoggingHandler()])
 #### /print debug information to stdout
 
+dataset = "nfcorpus"
+
 #### Download nfcorpus.zip dataset and unzip the dataset
-dataset = "nfcorpus.zip"
-url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}".format(dataset)
+url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
 out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
 data_path = util.download_and_unzip(url, out_dir)
 
-#### Provide the data_path where nfcorpus has been downloaded and unzipped
-corpus, queries, qrels = GenericDataLoader(data_path).load(split="test")
+#### Provide the data path where nfcorpus has been downloaded and unzipped to the data loader
+# data folder would contain these files: 
+# (1) nfcorpus/corpus.jsonl  (format: jsonlines)
+# (2) nfcorpus/queries.jsonl (format: jsonlines)
+# (3) nfcorpus/qrels/test.tsv (format: tsv ("\t"))
 
+corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
 
-#### Defining our FAISS index
-#### Number of clusters used for faiss. Select a value 4*sqrt(N) to 16*sqrt(N)
-#### https://github.com/facebookresearch/faiss/wiki/Guidelines-to-choose-an-index
-n_clusters = 1024
+# Dense Retrieval using Different Faiss Indexes (Flat or ANN) ####
+# Provide any Sentence-Transformer or Dense Retriever model.
 
-#### Number of clusters to explorer at search time. We will search for nearest neighbors in 3 clusters.
-nprobe = 3
+model_path = "msmarco-distilbert-base-tas-b"
+model = models.SentenceBERT(model_path)
 
-model = DRFS(model=models.SentenceBERT("distilroberta-base-msmarco-v2"), n_clusters=n_clusters, nprobe=nprobe)
-retriever = EvaluateRetrieval(model)
+########################################################
+#### FLATIP: Flat Inner Product (Exhaustive Search) ####
+########################################################
+
+faiss_search = FlatIPFaissSearch(model, 
+                                 batch_size=128)
+
+######################################################
+#### PQ: Product Quantization (Exhaustive Search) ####
+######################################################
+
+# faiss_search = PQFaissSearch(model, 
+#                              batch_size=128, 
+#                              num_of_centroids=96, 
+#                              code_size=8)
+
+#####################################################
+#### HNSW: Approximate Nearest Neighbours Search ####
+#####################################################
+
+# faiss_search = HNSWFaissSearch(model, 
+#                                batch_size=128, 
+#                                hnsw_store_n=512, 
+#                                hnsw_ef_search=128,
+#                                hnsw_ef_construction=200)
+
+#### Load faiss index from file or disk ####
+# We need two files to be present within the input_dir!
+# 1. input_dir/{prefix}.{ext}.faiss => which loads the faiss index.
+# 2. input_dir/{prefix}.{ext}.faiss => which loads mapping of ids i.e. (beir-doc-id \t faiss-doc-id).
+
+prefix = "my-index"       # (default value)
+ext = "flat"              # or "pq", "hnsw"
+input_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "faiss-index")
+
+if os.path.isdir(input_dir):
+    faiss_search.load(input_dir=input_dir, prefix=prefix, ext=ext)
 
 #### Retrieve dense results (format of results is identical to qrels)
+retriever = EvaluateRetrieval(faiss_search, score_function="dot") # or "cos_sim"
 results = retriever.retrieve(corpus, queries)
 
+### Save faiss index into file or disk ####
+# Unfortunately faiss only supports integer doc-ids, We need save two files in output_dir.
+# 1. output_dir/{prefix}.{ext}.faiss => which saves the faiss index.
+# 2. output_dir/{prefix}.{ext}.faiss => which saves mapping of ids i.e. (beir-doc-id \t faiss-doc-id).
+
+prefix = "my-index"      # (default value)
+ext = "flat"             # or "pq", "hnsw" 
+output_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "faiss-index")
+
+os.makedirs(output_dir, exist_ok=True)
+faiss_search.save(output_dir=output_dir, prefix=prefix, ext=ext)
+
 #### Evaluate your retrieval using NDCG@k, MAP@K ...
+
+logging.info("Retriever evaluation for k in: {}".format(retriever.k_values))
 ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
 
-#### Retrieval Example ####
-query_id, scores_dict = random.choice(list(results.items()))
-print("Query : %s\n" % queries[query_id])
+mrr = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="mrr")
+recall_cap = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="r_cap")
+hole = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="hole")
 
-scores = sorted(scores_dict.items(), key=lambda item: item[1], reverse=True)
-for rank in range(10):
-    doc_id = scores[rank][0]
-    print("Doc %d: %s [%s] - %s\n" % (rank+1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
+#### Print top-k documents retrieved ####
+top_k = 10
+
+query_id, ranking_scores = random.choice(list(results.items()))
+scores_sorted = sorted(ranking_scores.items(), key=lambda item: item[1], reverse=True)
+logging.info("Query : %s\n" % queries[query_id])
+
+for rank in range(top_k):
+    doc_id = scores_sorted[rank][0]
+    # Format: Rank x: ID [Title] Body
+    logging.info("Rank %d: %s [%s] - %s\n" % (rank+1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))

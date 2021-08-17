045f831636860c68a7e695c1f08726b03a7dbf95
Nandan Thakur <nandant@gmail.com>
2021-08-17 20:24:38 +0200

    fix: removed local paths from script

--- examples/retrieval/training/train_msmarco_v3_margin_MSE.py
+++ examples/retrieval/training/train_msmarco_v3_margin_MSE.py
@@ -46,11 +46,9 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 
 #### Download msmarco.zip dataset and unzip the dataset
 dataset = "msmarco"
-# url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
-# out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
-# data_path = util.download_and_unzip(url, out_dir)
-
-data_path = "/home/ukp/thakur/projects/sbert_retriever/datasets-new/msmarco"
+url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
+out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
+data_path = util.download_and_unzip(url, out_dir)
 
 ### Load BEIR MSMARCO training dataset, this will be used for query and corpus for reference.
 corpus, queries, _ = GenericDataLoader(data_path).load(split="train")

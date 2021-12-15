7c1167de1896d5764c8ccc19b7770d7f7a445b6e
Nthakur20 <nandant@gmail.com>
2021-12-15 02:06:53 +0100

    added new models: uniCOIL, SPLADE and TLDR within BEIR

--- beir/retrieval/models/__init__.py
+++ beir/retrieval/models/__init__.py
@@ -3,3 +3,6 @@ from .use_qa import UseQA
 from .sparta import SPARTA
 from .dpr import DPR
 from .bpr import BinarySentenceBERT
+from .unicoil import UniCOIL
+from .splade import SPLADE
+from .tldr import TLDR

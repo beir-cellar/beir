8197a736b8ea60dfd4816ac1129b4d871187a9ac
tmartins <thigm85@gmail.com>
2022-01-24 14:06:56 -0300

    include deployment parameters. Fix result object.

--- examples/benchmarking/benchmark_lexical_vespa.py
+++ examples/benchmarking/benchmark_lexical_vespa.py
@@ -26,6 +26,7 @@ def prepare_data(data_path):
 
 def get_search_results(dataset_name, corpus, queries, qrels):
     initialize = True
+    deployment_parameters = None
     metrics = []
     for match_phase in ["or", "weak_and"]:
         for rank_phase in ["bm25", "native_rank"]:
@@ -34,30 +35,32 @@ def get_search_results(dataset_name, corpus, queries, qrels):
                 match_phase=match_phase,
                 rank_phase=rank_phase,
                 initialize=initialize,
+                deployment_parameters=deployment_parameters,
             )
             initialize = False
+            deployment_parameters = {"url": "http://localhost", "port": 8089}
             retriever = EvaluateRetrieval(model)
             results = retriever.retrieve(corpus, queries)
             ndcg, _map, recall, precision = retriever.evaluate(
                 qrels, results, retriever.k_values
             )
-            metrics.append(
-                {
-                    "dataset_name": dataset_name,
-                    "match_phase": match_phase,
-                    "rank_phase": rank_phase,
-                    "ndcg": ndcg,
-                    "map": _map,
-                    "recall": recall,
-                    "precision": precision,
-                }
-            )
+            metric = {
+                "dataset_name": dataset_name,
+                "match_phase": match_phase,
+                "rank_phase": rank_phase,
+            }
+            metric.update(ndcg)
+            metric.update(_map)
+            metric.update(recall)
+            metric.update(precision)
+            metrics.append(metric)
     return metrics
 
 
 def benchmark_vespa_lexical(data_dir, dataset_names):
     result = []
     for dataset_name in dataset_names:
+        print("Dataset: {}".format(dataset_name))
         data_path = download_and_unzip_dataset(
             data_dir=data_dir, dataset_name=dataset_name
         )
@@ -82,6 +85,6 @@ def benchmark_vespa_lexical(data_dir, dataset_names):
 if __name__ == "__main__":
 
     data_dir = "/Users/tmartins/projects/sw/beir/data"
-    dataset_names = ["scifact"]
+    dataset_names = ["scifact", "nfcorpus"]
 
     result = benchmark_vespa_lexical(data_dir=data_dir, dataset_names=dataset_names)

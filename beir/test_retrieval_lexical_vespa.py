caf5131c7b73ab20b809d4f0ec8ce6c507dfa4aa
tmartins <thigm85@gmail.com>
2022-01-24 14:31:05 -0300

    use remove app method on the unit tests

--- beir/test_retrieval_lexical_vespa.py
+++ beir/test_retrieval_lexical_vespa.py
@@ -1,6 +1,4 @@
 import unittest
-import shutil
-import docker
 from beir.retrieval.search.lexical.vespa_search import VespaLexicalSearch
 from beir.retrieval.evaluation import EvaluateRetrieval
 
@@ -16,54 +14,52 @@ class TestVespaSearch(unittest.TestCase):
         self.queries = {"1": "this is query 1", "2": "this is query 2"}
 
     def test_or_bm25(self):
-        model = VespaLexicalSearch(application_name=self.application_name, initialize=True)
-        retriever = EvaluateRetrieval(model)
+        self.model = VespaLexicalSearch(
+            application_name=self.application_name, initialize=True
+        )
+        retriever = EvaluateRetrieval(self.model)
         results = retriever.retrieve(corpus=self.corpus, queries=self.queries)
         self.assertEqual({"1", "2"}, set(results.keys()))
         for query_id in results.keys():
             self.assertEqual({"1", "2", "3"}, set(results[query_id].keys()))
 
     def test_or_native_rank(self):
-        model = VespaLexicalSearch(
+        self.model = VespaLexicalSearch(
             application_name=self.application_name,
             initialize=True,
             match_phase="or",
             rank_phase="native_rank",
         )
-        retriever = EvaluateRetrieval(model)
+        retriever = EvaluateRetrieval(self.model)
         results = retriever.retrieve(corpus=self.corpus, queries=self.queries)
         self.assertEqual({"1", "2"}, set(results.keys()))
         for query_id in results.keys():
             self.assertEqual({"1", "2", "3"}, set(results[query_id].keys()))
 
     def test_weakand_bm25(self):
-        model = VespaLexicalSearch(
+        self.model = VespaLexicalSearch(
             application_name=self.application_name,
             initialize=True,
             match_phase="weak_and",
         )
-        retriever = EvaluateRetrieval(model)
+        retriever = EvaluateRetrieval(self.model)
         results = retriever.retrieve(corpus=self.corpus, queries=self.queries)
         self.assertEqual({"1", "2"}, set(results.keys()))
         for query_id in results.keys():
             self.assertEqual({"1", "2", "3"}, set(results[query_id].keys()))
 
     def test_weakand_native_rank(self):
-        model = VespaLexicalSearch(
+        self.model = VespaLexicalSearch(
             application_name=self.application_name,
             initialize=True,
             match_phase="weak_and",
-            rank_phase="native_rank"
+            rank_phase="native_rank",
         )
-        retriever = EvaluateRetrieval(model)
+        retriever = EvaluateRetrieval(self.model)
         results = retriever.retrieve(corpus=self.corpus, queries=self.queries)
         self.assertEqual({"1", "2"}, set(results.keys()))
         for query_id in results.keys():
             self.assertEqual({"1", "2", "3"}, set(results[query_id].keys()))
 
     def tearDown(self) -> None:
-        shutil.rmtree(self.application_name, ignore_errors=True)
-        client = docker.from_env()
-        container = client.containers.get(self.application_name)
-        container.stop()
-        container.remove()
+        self.model.remove_app()

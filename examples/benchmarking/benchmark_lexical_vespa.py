eac2910ce04c7249477e0af777c6ebe7e85b2ef9
tmartins <thigm85@gmail.com>
2022-01-24 10:34:57 -0300

    add vespa benchmark script

---
+++ examples/benchmarking/benchmark_lexical_vespa.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python3
+
+import os
+from beir import util
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval.search.lexical.vespa_search import VespaLexicalSearch
+from beir.retrieval.evaluation import EvaluateRetrieval
+from pandas import DataFrame
+
+
+def download_and_unzip_dataset(data_dir, dataset_name):
+    url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(
+        dataset_name
+    )
+    data_path = util.download_and_unzip(url, data_dir)
+    print("Dataset downloaded here: {}".format(data_path))
+    return data_path
+
+
+def prepare_data(data_path):
+    corpus, queries, qrels = GenericDataLoader(data_path).load(
+        split="test"
+    )  # or split = "train" or "dev"
+    return corpus, queries, qrels
+
+
+def get_search_results(dataset_name, corpus, queries, qrels):
+    initialize = True
+    metrics = []
+    for match_phase in ["or", "weak_and"]:
+        for rank_phase in ["bm25", "native_rank"]:
+            model = VespaLexicalSearch(
+                application_name=dataset_name,
+                match_phase=match_phase,
+                rank_phase=rank_phase,
+                initialize=initialize,
+            )
+            initialize = False
+            retriever = EvaluateRetrieval(model)
+            results = retriever.retrieve(corpus, queries)
+            ndcg, _map, recall, precision = retriever.evaluate(
+                qrels, results, retriever.k_values
+            )
+            metrics.append(
+                {
+                    "dataset_name": dataset_name,
+                    "match_phase": match_phase,
+                    "rank_phase": rank_phase,
+                    "ndcg": ndcg,
+                    "map": _map,
+                    "recall": recall,
+                    "precision": precision,
+                }
+            )
+    return metrics
+
+
+def benchmark_vespa_lexical(data_dir, dataset_names):
+    result = []
+    for dataset_name in dataset_names:
+        data_path = download_and_unzip_dataset(
+            data_dir=data_dir, dataset_name=dataset_name
+        )
+        corpus, queries, qrels = prepare_data(data_path=data_path)
+        metrics = get_search_results(
+            dataset_name=dataset_name, corpus=corpus, queries=queries, qrels=qrels
+        )
+        output_file = os.path.join(data_dir, "metrics.csv")
+        if os.path.isfile(output_file):
+            DataFrame.from_records(metrics).to_csv(
+                output_file, mode="a", header=False, index=False
+            )
+        else:
+            DataFrame.from_records(metrics).to_csv(
+                output_file, mode="w", header=True, index=False
+            )
+        print(metrics)
+        result.extend(metrics)
+    return result
+
+
+if __name__ == "__main__":
+
+    data_dir = "/Users/tmartins/projects/sw/beir/data"
+    dataset_names = ["scifact"]
+
+    result = benchmark_vespa_lexical(data_dir=data_dir, dataset_names=dataset_names)

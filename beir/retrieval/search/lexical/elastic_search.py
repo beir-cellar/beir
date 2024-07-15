57913ff23bb230879a2fd84019e09f6edb0d7089
kaengreg <kaengreg654@gmail.com>
2024-07-15 18:18:27 +0300

    analyzer returned

--- beir/retrieval/search/lexical/elastic_search.py
+++ beir/retrieval/search/lexical/elastic_search.py
@@ -75,8 +75,8 @@ class ElasticSearch(object):
                 mapping = {
                     "mappings": {
                         "properties": {
-                            self.title_key: {"type": "text"},
-                            self.text_key: {"type": "text"}
+                            self.title_key: {"type": "text", "analyzer": self.language},
+                            self.text_key: {"type": "text", "analyzer": self.language}
                         }}}
             else:
                 mapping = {
@@ -85,8 +85,8 @@ class ElasticSearch(object):
                     },
                     "mappings": {
                         "properties": {
-                            self.title_key: {"type": "text"},
-                            self.text_key: {"type": "text"}
+                            self.title_key: {"type": "text", "analyzer": self.language},
+                            self.text_key: {"type": "text", "analyzer": self.language}
                         }}}
 
             self.es.indices.create(index=self.index_name, body=mapping,

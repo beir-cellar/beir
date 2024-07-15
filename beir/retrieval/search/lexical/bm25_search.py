fb789219f97577404d4e5e578ba12bed48159893
kaengreg <kaengreg654@gmail.com>
2024-07-15 17:32:48 +0300

    indexing fix

--- beir/retrieval/search/lexical/bm25_search.py
+++ beir/retrieval/search/lexical/bm25_search.py
@@ -70,7 +70,7 @@ class BM25Search(BaseSearch):
         # dictionary structure = {_id: {title_key: title, text_key: text}}l
         dictionary = {idx: {
             self.config["keys"]["title"]: corpus[idx].get("title", None), 
-            self.config["keys"]["body"]: corpus[idx].get("processed_text", None)
+            self.config["keys"]["body"]: corpus[idx].get("text", None)
             } for idx in list(corpus.keys())
         }
         self.es.bulk_add_to_index(

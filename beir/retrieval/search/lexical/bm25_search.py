c9f7e642d4b61cc5118a851c19b577dfee81a286
kaengreg <kaengreg654@gmail.com>
2024-07-15 13:53:21 +0300

    Default settings except analyzer returned

--- beir/retrieval/search/lexical/bm25_search.py
+++ beir/retrieval/search/lexical/bm25_search.py
@@ -11,7 +11,7 @@ class BM25Search(BaseSearch):
     def __init__(self, index_name: str, hostname: str = "localhost", keys: Dict[str, str] = {"title": "title", "body": "txt"}, language: str = "english",
                  batch_size: int = 128, timeout: int = 1000, retry_on_timeout: bool = True, maxsize: int = 24, number_of_shards: int = "default",
                  initialize: bool = True, sleep_for: int = 2):
-        keys["body"] = "processed_text"
+        #keys["body"] = "processed_text"
         self.results = {}
         self.batch_size = batch_size
         self.initialize = initialize

8998e80d47f37bf3ee2fc1e3a8f146b742d3c914
Nandan Thakur <nandant@gmail.com>
2025-06-03 12:24:29 -0400

    feature: now huggingface supports multi-gpu embedding eval

--- beir/retrieval/models/util.py
+++ beir/retrieval/models/util.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from collections.abc import Mapping
+
+import torch
+
 
 def extract_corpus_sentences(corpus: list[dict[str, str]] | dict[str, list] | list[str], sep: str) -> list[str]:
     """Extracts sentences from the corpus"""
@@ -17,3 +21,25 @@ def extract_corpus_sentences(corpus: list[dict[str, str]] | dict[str, list] | li
                 (doc["title"] + sep + doc["text"]).strip() if "title" in doc else doc["text"].strip() for doc in corpus
             ]
     return sentences
+
+
+# Taken from https://github.com/microsoft/unilm/blob/master/e5/utils.py#L24
+def move_to_cuda(sample: dict | list | tuple | torch.Tensor) -> dict | list | tuple | torch.Tensor:
+    if len(sample) == 0:
+        return {}
+
+    def _move_to_cuda(maybe_tensor):
+        if torch.is_tensor(maybe_tensor):
+            return maybe_tensor.cuda(non_blocking=True)
+        elif isinstance(maybe_tensor, dict):
+            return {key: _move_to_cuda(value) for key, value in maybe_tensor.items()}
+        elif isinstance(maybe_tensor, list):
+            return [_move_to_cuda(x) for x in maybe_tensor]
+        elif isinstance(maybe_tensor, tuple):
+            return tuple([_move_to_cuda(x) for x in maybe_tensor])
+        elif isinstance(maybe_tensor, Mapping):
+            return type(maybe_tensor)({k: _move_to_cuda(v) for k, v in maybe_tensor.items()})
+        else:
+            return maybe_tensor
+
+    return _move_to_cuda(sample)
